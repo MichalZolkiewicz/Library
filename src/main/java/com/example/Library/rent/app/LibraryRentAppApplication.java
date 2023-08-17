21cee521e4b3bf242db9f0e8d9367df6fb2cf113
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-08-17 17:44:31 +0200

    main clasess

---
+++ src/main/java/com/example/Library/rent/app/LibraryRentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Library.rent.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryRentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Library/rent/app/LibraryRentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Library.rent.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryRentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
