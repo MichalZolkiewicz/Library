11953d573a9a6f3958ecfe04a04c5b4206fbb58c
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-08-18 19:02:55 +0200

    column annotation added

--- src/main/java/com/example/Library/rent/app/Core/Owner.java
+++ src/main/java/com/example/Library/rent/app/Core/Owner.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Setter
 @AllArgsConstructor
@@ -20,10 +17,12 @@ public class Owner {
     private String name;
 
     @Id
+    @Column(name = "ID")
     public int getId() {
         return id;
     }
 
+    @Column (name = "NAME")
     public String getName() {
         return name;
     }
